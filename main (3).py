4449033bc055155e7fd7d33aae8e08d3e6c7f7df
Abi1518 <144206084+Abi1518@users.noreply.github.com>
2023-09-30 19:44:28 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,19 @@
+#challenge 2.2
+#defining class player
+class Player:
+    def play(self):
+       print("The player is playing cricket.")
+#defining class batsman
+class Batsman(Player):
+    def play(self) :
+       print("The batsman is batting.")
+#defining class bowler
+class Bowler(Player):
+    def play(self) :
+       print("The bowler is bowling.")
+#creating an instance of the class
+batsman=Batsman()
+bowler=Bowler()
+ 
+batsman.play()
+bowler.play()
